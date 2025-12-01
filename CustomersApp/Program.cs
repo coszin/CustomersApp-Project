4625277abd22dbf59877d6387b378db613716475
Oscar Svahn <oscar@svahndata.se>
2025-12-01 14:49:36 +0100

    Add project files.

---
+++ CustomersApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomersApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
